19206216ca57df6697ce929c6cffd4ad12c1b3ee
akazad1235 <akazad914@gmail.com>
2022-10-22 13:09:23 +0600

    fake product create done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
 
 const Shop = () => {
+    const[products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('fakeProduct.json')
+        .then(res => res.json())
+        .then(data =>setProducts(data))
+    }, [])
+    console.log(products);
     return (
-        <div className='d-flex justify-content-between m-3'>
-            <Product />
-            <Cart />
+        <div className='row'>
+            <div className="col-md-8">
+                <div className='d-flex'>
+                    <Product />
+                </div>
+            </div>
+            <div className="col-md-4">
+                <Cart />
+            </div>
         </div>
     );
 };
